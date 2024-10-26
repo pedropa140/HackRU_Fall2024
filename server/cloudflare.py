ef0c1e3a49edfdef5362f6e39e1436a3dfd90946
Pedro A. Pajarillo Jr <pedropa140@gmail.com>
2024-10-26 15:19:27 -0400

    Cloudflare Works

--- server/cloudflare.py
+++ server/cloudflare.py
@@ -9,14 +9,7 @@ API_BASE_URL = "https://api.cloudflare.com/client/v4/accounts/3edf18a2c5178542d9
 
 def run(model, inputs):
     
-    headers = {"Authorization": "bUs2t9mXu-Ylihaz6EOoISSKlsFn0VpFuliPX-XP"}
+    headers = {"Authorization": CLOUDFLARE_API}
     input = { "messages": inputs }
     response = requests.post(f"{API_BASE_URL}{model}", headers=headers, json=input)
     return response.json()
-
-inputs = [
-    { "role": "system", "content": "You are a friendly assistan that helps write stories" },
-    { "role": "user", "content": "Write a short story about a llama that goes on a journey to find an orange cloud "}
-]
-output = run("@cf/meta/llama-3-8b-instruct", inputs)
-print(output)
